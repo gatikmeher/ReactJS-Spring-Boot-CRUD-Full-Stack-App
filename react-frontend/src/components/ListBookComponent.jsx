f8151b6479acd41e3dafcce91f9e7a7f2af08b02
Shri-Tambaskar <shrikant.tambaskar@mkpits.in>
2024-02-22 17:22:17 +0530

    Pagination Added

--- react-frontend/src/components/ListBookComponent.jsx
+++ react-frontend/src/components/ListBookComponent.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import BookService from "../services/BookService";
 import "font-awesome/css/font-awesome.min.css";
 import Dropdown from "react-bootstrap/Dropdown";
+import { Table, Pagination } from "react-bootstrap";
 
 class ListBookComponent extends Component {
   constructor(props) {
@@ -9,8 +10,10 @@ class ListBookComponent extends Component {
 
     this.state = {
       books: [],
+      booksToBeShown: [],
+      currentPage: 1,
     };
-    this.state.searchSelection = "title"
+    this.state.searchSelection = "title";
     this.addBook = this.addBook.bind(this);
     this.editBook = this.editBook.bind(this);
     this.deleteBook = this.deleteBook.bind(this);
@@ -33,29 +36,105 @@ class ListBookComponent extends Component {
 
   componentDidMount() {
     BookService.getBooks(null, null, 0).then((res) => {
-      this.setState({ books: res.data.content });
+      this.setState({
+        books: res.data.content,
+        pageSize: 10,
+        booksToBeShown: [],
+        pageArray: [],
+      });
+      this.calculatePaginationDetails(1);
     });
   }
 
+  // Pagination Implementation
+  calculatePaginationDetails = (page) => {
+    console.log(page);
+    let books = this.state.books;
+    let total = books.length;
+    let pages = Math.floor(books.length / this.state.pageSize + 1);
+    let firstPage = 1;
+    let lastPage = pages;
+    let pageArray = [];
+    let booksToBeShown = [];
+    let currentPage = 1;
+    if (page.toString().toLowerCase().indexOf("previous") > 0) {
+      currentPage = this.state.currentPage - 1;
+      if (currentPage < 1) {
+        currentPage = 1;
+      }
+    } else if (page.toString().toLowerCase().indexOf("next") > 0) {
+      currentPage = this.state.currentPage + 1;
+      if (currentPage > pages) {
+        currentPage = pages;
+      }
+    } else if (page.toString().toLowerCase().indexOf("first") > 0) {
+      currentPage = 1;
+    } else if (page.toString().toLowerCase().indexOf("last") > 0) {
+      currentPage = pages;
+    } else {
+      currentPage = parseInt(page);
+    }
+    console.log(parseInt(page));
+    console.log(currentPage);
+    for (let i = currentPage; i <= currentPage + 4; i++) {
+      if (i <= pages) pageArray.push(i);
+    }
+    let currentItemIndex = (currentPage - 1) * this.state.pageSize;
+    for (
+      let i = currentItemIndex;
+      i < currentItemIndex + 10 && i <= total - 1;
+      i++
+    ) {
+      booksToBeShown.push(books[i]);
+    }
+    let updatedState = {
+      booksToBeShown: booksToBeShown,
+      pageArray: pageArray,
+      firstPage: firstPage,
+      lastPage: lastPage,
+      currentPage: currentPage,
+    };
+    console.log(updatedState);
+    this.setState({
+      booksToBeShown: booksToBeShown,
+      pageArray: pageArray,
+      firstPage: firstPage,
+      lastPage: lastPage,
+      currentPage: currentPage,
+    });
+  };
+
+  // Handle Pagination
+  handlePagination = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    if (e.target.text !== undefined) {
+      this.calculatePaginationDetails(e.target.text);
+    }
+  };
+
   searchBook() {
-    console.log("searchSelection: " + this.state.searchSelection)
-    BookService.getBooks(this.state.searchSelection, this.state.searchText, this.state.page).then((res) => {
+    console.log("searchSelection: " + this.state.searchSelection);
+    BookService.getBooks(
+      this.state.searchSelection,
+      this.state.searchText,
+      this.state.page
+    ).then((res) => {
       this.setState({ books: res.data.content });
     });
   }
 
   searchSelectionChanged = (event) => {
-    console.log("searchSelection: " + event.target.value)
-    this.state.searchSelection = event.target.value
+    console.log("searchSelection: " + event.target.value);
+    this.state.searchSelection = event.target.value;
     // this.setState({searchSelection: event.target.value});
-  }
+  };
 
   searchTextChanged = (event) => {
-    console.log("searchText: " + event.target.value)
-    this.state.searchText = event.target.value
+    console.log("searchText: " + event.target.value);
+    this.state.searchText = event.target.value;
     // this.setState({searchSelection: event.target.value});
-  }
-
+  };
 
   addBook() {
     this.props.history.push("/add-book/_add");
@@ -85,12 +164,12 @@ class ListBookComponent extends Component {
             </Dropdown.Menu>
           </Dropdown> */}
           <div>
-          <select className="form-control" >
-            <option value="JSON" >JSON</option>
-            <option value="XML" >XML</option>
-            <option value="CSV" >CSV</option>
-          </select>
-          </div>          
+            <select className="form-control">
+              <option value="JSON">JSON</option>
+              <option value="XML">XML</option>
+              <option value="CSV">CSV</option>
+            </select>
+          </div>
         </div>
 
         <br></br>
@@ -108,11 +187,39 @@ class ListBookComponent extends Component {
               </Dropdown.Menu>
             </Dropdown> */}
             <div>
-            <select className="form-control" value={this.state.searchSelection}  onChange={this.searchSelectionChanged}>
-              <option value="title" name="title" selected={ this.state.searchSelection === 'title' ? 'selected' : false } >Title</option>
-              <option value="genres" name="genres"  selected={ this.state.searchSelection === 'genres' ? 'selected' : false }>Genres</option>
-              <option value="date" name="date" selected={ this.state.searchSelection === 'date' ? 'selected' : false }>Date</option>
-            </select>
+              <select
+                className="form-control"
+                value={this.state.searchSelection}
+                onChange={this.searchSelectionChanged}
+              >
+                <option
+                  value="title"
+                  name="title"
+                  selected={
+                    this.state.searchSelection === "title" ? "selected" : false
+                  }
+                >
+                  Title
+                </option>
+                <option
+                  value="genres"
+                  name="genres"
+                  selected={
+                    this.state.searchSelection === "genres" ? "selected" : false
+                  }
+                >
+                  Genres
+                </option>
+                <option
+                  value="date"
+                  name="date"
+                  selected={
+                    this.state.searchSelection === "date" ? "selected" : false
+                  }
+                >
+                  Date
+                </option>
+              </select>
             </div>
             &nbsp;&nbsp;&nbsp;
             <input
@@ -123,11 +230,17 @@ class ListBookComponent extends Component {
               onChange={this.searchTextChanged}
             />{" "}
             &nbsp;&nbsp;&nbsp;
-            <button className="btn btn-primary" onClick={() => this.searchBook()}> Search</button>{" "}
+            <button
+              className="btn btn-primary"
+              onClick={() => this.searchBook()}
+            >
+              {" "}
+              Search
+            </button>{" "}
             &nbsp;&nbsp;&nbsp;
           </div>
         </div>
-        <div>
+        {/* <div>
           <button className="btn btn-primary" style={{ marginLeft: "230px" }}>
             Prev
           </button>
@@ -135,6 +248,26 @@ class ListBookComponent extends Component {
           <button className="btn btn-primary" style={{ marginLeft: "1px" }}>
             Next
           </button>
+        </div> */}
+
+        <div style={{ marginLeft: "340px" }}>
+          <Pagination>
+            <Pagination.First onClick={(e) => this.handlePagination(e)} />
+            <Pagination.Prev onClick={(e) => this.handlePagination(e)} />
+            {this.state.pageArray &&
+              this.state.pageArray.length &&
+              this.state.pageArray.map((item) => (
+                <Pagination.Item
+                  key={item}
+                  onClick={(e) => this.handlePagination(e)}
+                  active={this.state.currentPage === item}
+                >
+                  {item}
+                </Pagination.Item>
+              ))}
+            <Pagination.Next onClick={(e) => this.handlePagination(e)} />
+            <Pagination.Last onClick={(e) => this.handlePagination(e)} />
+          </Pagination>
         </div>
         <div className="row">
           <table className="table table-striped table-bordered">
@@ -157,12 +290,12 @@ class ListBookComponent extends Component {
                   <td>
                     {" "}
                     <p
-                      // style={{
-                      //   WebkitLineClamp: 3,
-                      //   WebkitBoxOrient: "vertical",
-                      //   overflow: "hidden",
-                      //   display: "-webkit-box",
-                      // }}
+                    // style={{
+                    //   WebkitLineClamp: 3,
+                    //   WebkitBoxOrient: "vertical",
+                    //   overflow: "hidden",
+                    //   display: "-webkit-box",
+                    // }}
                     >
                       {book.genres}
                     </p>
@@ -179,12 +312,12 @@ class ListBookComponent extends Component {
                   <td>
                     {" "}
                     <p
-                      // style={{
-                      //   WebkitLineClamp: 3,
-                      //   WebkitBoxOrient: "vertical",
-                      //   overflow: "hidden",
-                      //   display: "-webkit-box",
-                      // }}
+                    // style={{
+                    //   WebkitLineClamp: 3,
+                    //   WebkitBoxOrient: "vertical",
+                    //   overflow: "hidden",
+                    //   display: "-webkit-box",
+                    // }}
                     >
                       {book.characters}
                     </p>
@@ -201,12 +334,12 @@ class ListBookComponent extends Component {
                   <td>
                     {" "}
                     <p
-                      // style={{
-                      //   WebkitLineClamp: 3,
-                      //   WebkitBoxOrient: "vertical",
-                      //   overflow: "hidden",
-                      //   display: "-webkit-box",
-                      // }}
+                    // style={{
+                    //   WebkitLineClamp: 3,
+                    //   WebkitBoxOrient: "vertical",
+                    //   overflow: "hidden",
+                    //   display: "-webkit-box",
+                    // }}
                     >
                       {book.synopsis}
                     </p>
